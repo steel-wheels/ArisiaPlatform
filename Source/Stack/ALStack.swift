70940a397c8c51f7e16d4a3da2d4516b0bbc512a
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2025-07-13 21:59:40 +0900

    ALStack: Add load frame method

--- Source/Stack/ALStack.swift
+++ Source/Stack/ALStack.swift
@@ -12,17 +12,41 @@ public class ALStack
 {
         private var mPackageDirectory:          URL
         private var mFrameScriptURLs:           Array<URL>
+        private var mFrameTable:                Dictionary<String, ALFrame>
 
         public var frameScriptURLs: Array<URL> { get{ return mFrameScriptURLs }}
 
         public init(packageDirectory pkgdir: URL) {
                 mPackageDirectory       = pkgdir
                 mFrameScriptURLs        = []
+                mFrameTable             = [:]
         }
 
         public func add(frameScriptPath path: String) {
                 let path = mPackageDirectory.appending(path: path)
                 mFrameScriptURLs.append(path)
+        }
+
+        public func loadFrame(at index: Int) -> Result<ALFrame, NSError> {
+                guard index < mFrameScriptURLs.count else {
+                        let err = MIError.error(errorCode: .fileError, message: "No frame at \(index)")
+                        return .failure(err)
+                }
+                /* load from cache */
+                let url = mFrameScriptURLs[index]
+                if let frame = mFrameTable[url.path] {
+                        return .success(frame)
+                }
+                /* allocate frame */
+                let text: String
+                do {
+                        text = try String(contentsOf: url, encoding: .utf8)
+                } catch {
+                        let err = MIError.error(errorCode: .fileError, message: "Failed to load script from \(url.path)")
+                        return .failure(err)
+                }
+                NSLog("(\(#function): loaded text: \(text)")
+                return .success(ALFrame())
         }
 }
 
